3bc0e3407bda92a830ba71cb6d96d00630a622ad
Erik <78443570+thekhachik@users.noreply.github.com>
2021-09-10 21:15:42 +0300

    Create lesson2_5.java

---
+++ lesson2_5.java
@@ -0,0 +1,39 @@
+import java.lang.System.*;
+
+public class Main{
+      
+    public static void main(String[] args) {
+         
+        Person tom = new Person(); //вызов конструтокра
+        Person bob = new Person(); //вызов конструтокра
+        
+        tom.displayId();
+        bob.displayId();
+        System.out.printf("Main: %d \n", tom.counter); 
+        System.out.printf("Main: %d \n", Person.counter);
+         
+        // изменяем Person.counter
+        Person.counter = 8;
+        
+        System.out.printf("Main: %d \n", bob.counter); 
+        
+        Person sam = new Person(); //вызов конструтокра
+        sam.displayId();
+        
+        System.out.printf("Main: %d \n", sam.counter); 
+        
+    }
+}
+class Person{
+    
+    public static int counter=1;
+     
+    Person(){
+        counter++;
+    }
+    
+    public void displayId(){
+     
+        System.out.printf("Person: %d \n", counter);
+    }
+}
