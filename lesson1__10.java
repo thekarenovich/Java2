fe2c515cd9964305e6e87181d7fbde3208cd438e
Erik <78443570+thekhachik@users.noreply.github.com>
2021-09-27 00:26:18 +0300

    Create lesson1__10.java

---
+++ lesson1__10.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+class Main {
+    public static void main(String[] args) {
+        
+        int array[] = {1, 5, 4, 3, 7}; 
+        
+        // System.out.println(array); // - неправильный вывод массива 
+        
+        System.out.println(Arrays.toString(array)); // печатаем массив "правильно" методом toString
+        
+        Arrays.sort(array, 0, 4); //сортируем весь массив от нулевого до четвёртого члена
+        // в данном случае, весь массив отсортировался
+        
+        System.out.println(Arrays.toString(array));//выводим отсортированный массив на экран
+        
+        System.out.println(Arrays.binarySearch(array, 5));
+        // ищем key - число 5 в отсортированном массиве.
+        //метод binarySearch выдаст индекс элемента остортированного массива, в котором "спрятано" искомое число
+        
+        System.out.println(Arrays.binarySearch(array, 0));
+        //а теперь попробуем найти число, которого в массиве нет
+
+    }
+}
