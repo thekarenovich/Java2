89d3ed003ad0e467aec734fc73d24008bfa68717
Erik <78443570+thekhachik@users.noreply.github.com>
2021-09-27 14:37:16 +0300

    Create lesson1_11.java

---
+++ lesson1_11.java
@@ -0,0 +1,12 @@
+public class Main
+{
+	public static void main(String[] args) {
+	    Integer i = new Integer(1200);
+	    Double d = new Double(12.45);
+	    Float f = new Float(10.2);
+	    Boolean b = new Boolean(true);
+	    String s = new String("fdfvdfvdfv");
+	    
+		System.out.println(i + " " + d + " " + f + " " + b + " " + s);
+	}
+}
