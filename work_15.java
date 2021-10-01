d220d00d3282d0f814477447b0a83d0054818c28
Erik <78443570+thekhachik@users.noreply.github.com>
2021-10-01 23:58:35 +0300

    Create work_15.java

---
+++ work_15.java
@@ -0,0 +1,20 @@
+//Cортиврока 
+
+public class Main {
+    public static void main(String[] args) {
+
+        int a[]={1,9,23,40,0,1,0,12};
+        
+        for(int i=1; i<a.length; i++)
+            for(int j=0; j<(a.length-i); j++)
+                if(a[j]>a[j+1]) {
+                    x = a[j+1];
+                    a[j+1]=a[j];
+                    a[j] = x;
+                }
+
+        for(int i=0; i<a.length; i++)
+            System.out.print(a[i]+" ");
+
+    }
+}
